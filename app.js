ea5853d4f3d836935903b9ec6008ed6701dec38f
Natnael1alem <natnaelalem10@gmail.com>
2025-02-01 17:16:55 +0300

    all project functionalities in working state

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 require('dotenv').config(); // use the .env file
 
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
+const methodOverride = require('method-override');  
 const connectDB = require('./server/config/db');
 const session = require('express-session');
 const passport = require('passport');
@@ -25,6 +26,7 @@ app.use(passport.session());
 
 app.use(express.urlencoded({extended: true})) // make it url encoded
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.use(express.static('public')); // use static files
 
 // layout templating engine
--- server/controllers/dashboaredController.js
+++ server/controllers/dashboaredController.js
@@ -1,8 +1,11 @@
 const Note = require('../models/Note');
 const mongoose = require('mongoose');
 
+
+// GET /
+// Dashboard
 exports.dashboard = async(req, res) => {
-    let perPage = 8;
+    let perPage = 6;
     let page = req.query.page || 1;
 
     const locals = {
@@ -36,10 +39,114 @@ exports.dashboard = async(req, res) => {
             current: page,
             pages: Math.ceil(count / perPage),
         });
+    } catch (error) {
+        console.log(error);
+    }  
+}
 
+// GET /
+// View Specific Note
+exports.dashboardViewNote = async(req, res) => {
+    const note = await Note.findById({_id: req.params.id})
+    .where({ user: req.user.id}).lean();
+
+    if(note) {
+        res.render('dashboard/view-note', {
+            noteID: req.params.id,
+            note,
+            layout: '../views/layouts/dashboard'
+        });
+    } else {
+        res.send("Something went wrong.");
+    }
+}
+
+
+// PUT
+// UPDATE Specific Notes
+exports.dashboardUpdateNote = async(req, res) => {
+    try {
+        await Note.findOneAndUpdate(
+            {_id: req.params.id},
+            {title: req.body.title, body: req.body.body, updatedAt: Date.now()}
+        ).where({user: req.user.id});
+
+        res.redirect('/dashboard')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+// Delete
+// DELETE Note
+exports.dashboardDeleteNote = async(req, res) => {
+    try{
+
+        await Note.deleteOne({_id: req.params.id}).where({user: req.user.id});
+        res.redirect('/dashboard')
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+// Get
+// ADD Note
+exports.dashboardAddNote = async(req, res) => {
+    res.render('dashboard/add', {
+        layout: '../views/layouts/dashboard'
+    });
+}
+
+
+// POST
+// ADD NOTE SUBMIT
+exports.dashboardAddNoteSubmit = async(req, res) => {
+    try {
+        req.body.user = req.user.id;
+        await Note.create(req.body);
+        res.redirect('/dashboard');
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+// Get
+// Search
+exports.dashboardSearch = async(req, res) => {
+    try {
+        res.render('/dashboard/search', {
+            searchResults: '',
+            layout: '../views/layouts/dashboard'
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// POST
+// Search
+exports.dashboardSearchSubmit = async(req, res) => {
+    try {
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChars = searchTerm.replace(/[^a-zA-Z0-9 ]/g, "");
+
+        const searchResults = await Note.find({
+            $or: [
+            { title: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+            { body: { $regex: new RegExp(searchNoSpecialChars, "i") } },
+            ],
+        }).where({ user: req.user.id });
+
+        res.render("dashboard/search", {
+            searchResults,
+            layout: "../views/layouts/dashboard",
+        });
     } catch (error) {
         console.log(error);
     }
-
-    
 }
